7bf2ff65140645f33433b55fdb9b8ad2340fc1f8
okmon12 <143723986+okmon12@users.noreply.github.com>
2023-09-21 22:00:16 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,17 @@
 def index
   @message = Message.new
   @room = Room.find(params[:room_id])
+  @messages = @room.messages.includes(:user)
 end
 
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-    redirect_to room_messages_path(@room)
+      redirect_to room_messages_path(@room)
     else
-    render :index, status: :unprocessable_entity
+      @messages = @room.messages.includes(:user)
+      render :index, status: :unprocessable_entity
   end
 end
 
